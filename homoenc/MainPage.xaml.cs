9fdd1e37b52b03a79abeb634d2ad752a8b97c4cb
sinyee <sinyeekwek@gmail.com>
2022-03-21 14:43:01 +0800

    Initialise repo.

---
+++ homoenc/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.Research.SEAL;
+
+namespace homoenc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            EncryptionParameters parms = new EncryptionParameters(SchemeType.CKKS);
+
+        }
+    }
+}
